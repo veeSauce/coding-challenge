50416d32a588b91436b4dd87d030b1618f2e05b3
vince jabbal <vincejabbal@vinces-MBP.lan1>
2019-06-05 15:39:36 -0500

    pushing

--- repl.go
+++ repl.go
@@ -1,16 +1,18 @@
 package main
 
+// Simple key value store with nested transactions allowed
+
 import (
+	"PA/stack"
 	"bufio"
-	."fmt"
-	"github.com/golang-collections/collections/stack"
+	. "fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"strings"
 )
 
 type data struct {
-	Stack    *stack.Stack
+	// Stack stack.Stack
 	Command  []string
 	KeyStore map[string]string
 }
@@ -18,134 +20,122 @@ type data struct {
 func main() {
 
 	log.SetOutput(os.Stderr)
+	Stack := stack.New()
 
-	s := stack.New()
 	m := make(map[string]string)
 
-	d := &data{
-		s,
+	d := data{
+		// Stack
 		nil,
 		m,
 	}
 
 	for {
-		d.DoStuff()
+
+		reader := bufio.NewReader(os.Stdin)
+
+		Print(">")
+
+		// read in arguments
+		text, _ := reader.ReadString('\n')
+
+		// collect arguments in slice
+		text = strings.Replace(text, "\n", "", -1)
+		command := strings.Fields(text)
+		command[0] = strings.ToUpper(command[0])
+		d.Command = command
+
+		switch d.Command[0] {
+
+		case "WRITE":
+
+			if ok := d.Check(3); ok {
+				d.Write(Stack)
+
+			} else {
+				log.Println("write must have a key and value")
+			}
+
+		case "READ":
+
+			if ok := d.Check(2); ok {
+				d.Read()
+			} else {
+				log.Println("Read command requires a key")
+			}
+
+		case "DELETE":
+			if ok := d.Check(2); ok {
+				d.Delete()
+			} else {
+				log.Println("Delete command requires a key")
+			}
+
+		case "START":
+
+			// push the stack with updated key value map as a new Map not pointer to old one
+			newM := createNewMap()
+			for k, v := range d.KeyStore {
+				newM[k] = v
+			}
+			Stack.Push(newM)
+
+		case "ABORT":
+
+			// wipe out the current transaction
+			if Stack.Len() == 0 {
+				m := make(map[string]string)
+				d.KeyStore = m
+			} else {
+				Stack.Pop()
+				peekMap := Stack.Peek()
+				d.KeyStore = peekMap
+			}
+
+		case "COMMIT":
+
+			// pop stack and push a commit in popped index
+			Stack.Pop()
+			Stack.Pop()
+			newM := createNewMap()
+			for k, v := range d.KeyStore {
+				newM[k] = v
+			}
+			Stack.Push(newM)
+
+		case "QUIT":
+
+			log.Println("Exiting...")
+			os.Exit(0)
+
+		default:
+
+			// write error to stderr
+			log.Println("Invalid command")
+		}
 	}
 }
 
-func (d *data) DoStuff() {
-
-	reader := bufio.NewReader(os.Stdin)
-
-	Print(">")
-
-	// read in arguments
-	text, _ := reader.ReadString('\n')
-
-	// collect arguments in slice
-	text = strings.Replace(text, "\n", "", -1)
-	command := strings.Fields(text)
-	command[0] = strings.ToUpper(command[0])
-	d.Command = command
-
-	switch d.Command[0] {
-
-	case "WRITE":
-
-		if ok := d.Check(3); ok {
-			d.Write()
-
-		} else {
-			log.Println("write must have a key and value")
-		}
-
-	case "READ":
-
-		if ok := d.Check(2); ok {
-			d.Read()
-		} else {
-			log.Println("Read command requires a key")
-		}
-
-	case "DELETE":
-		if ok := d.Check(2); ok {
-			d.Delete()
-		} else {
-			log.Println("Delete command requires a key")
-		}
-
-	case "START":
-
-
-		// push the stack with updated key value pair transactions
-		d.Stack.Push(d.KeyStore)
-		Println("These are the values in keystore")
-		//d.KeyStore = d.Stack.Peek().(map[string]string)
-		Println("The length of the stack is: ",d.Stack.Len())
-
-
-	case "ABORT":
-		Println("length of stack is: ", d.Stack.Len())
-
-		// wipe out the current transaction
-		// if stack is 0 clear the map, if its 1 pop the stack then clear the map
-		//if d.Stack.Len() == 1 {
-		//	d.Stack.Pop()
-		//	m := make(map[string]string)
-		//	d.KeyStore = m
-		//
-		//} else
-		if d.Stack.Len() == 0 {
-			m := make(map[string]string)
-			d.KeyStore = m
-		} else {
-			d.Stack.Pop()
-
-			d.KeyStore = d.Stack.Peek().(map[string]string)
-			Print(d.KeyStore)
-			Println("after aborting length of stack is: ", d.Stack.Len())
-		}
-
-	case "COMMIT":
-
-		// pop stack and push a commit in popped index
-		d.Stack.Pop()
-		d.Stack.Pop()
-		d.Stack.Push(d.KeyStore)
-		//d.KeyStore = d.Stack.Peek().(map[string]string)
-
-
-
-	case "QUIT":
-
-		log.Println("Exiting...")
-		os.Exit(0)
-
-	default:
-
-		// write error to stderr
-		log.Println("Invalid command")
-
-	}
-
-}
-
-func (d *data) Write() {
+func (d *data) Write(Stack *stack.Stack) *stack.Stack {
 
 	// first write writes to stack as root store
-	if d.Stack.Len() == 0 && len(d.KeyStore) == 0{
+	if Stack.Len() == 0 && len(d.KeyStore) == 0 {
 		d.KeyStore[d.Command[1]] = d.Command[2]
-		d.Stack.Push(d.KeyStore)
 
+		newMap := make(map[string]string)
+		for k, v := range d.KeyStore {
+			newMap[k] = v
+		}
+		Stack.Push(newMap)
+		return Stack
 
 	}
-		// just a regular write
-		d.KeyStore[d.Command[1]] = d.Command[2]
-	}
+	// just a regular write
+	d.KeyStore[d.Command[1]] = d.Command[2]
+	return Stack
+}
 
-
-func (d *data) Read(){
-
+func (d *data) Read() {
 
 	if val, ok := d.KeyStore[d.Command[1]]; ok {
 		Println(val)
@@ -165,12 +155,14 @@ func (d *data) Check(reqLen int) bool {
 
 func (d *data) Delete() {
 
-	log.Println("delete called")
-
 	if _, ok := d.KeyStore[d.Command[1]]; ok {
-		Println("deleting: ", d.Command[1])
 		delete(d.KeyStore, d.Command[1])
 	} else {
 		log.Println("Key not found: ", d.Command[1])
 	}
+}
+
+func createNewMap() map[string]string {
+	new := make(map[string]string)
+	return new
 }
